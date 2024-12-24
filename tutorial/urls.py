304c7183fc462fc904c673bee055412e934795d9
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-24 09:49:12 +0700

    Rename project from quickstart to tutorial

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for quickstart project.
+URL configuration for tutorial project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
